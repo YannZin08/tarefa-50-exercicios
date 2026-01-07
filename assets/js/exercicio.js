213a160fc218f2c3ec264588cc22421e300178f8
Yann Felipe <yannzinfelipe@gmail.com>
2026-01-07 14:33:12 -0300

    adicionado pagina de exercicios js

---
+++ assets/js/exercicio.js
@@ -0,0 +1,44 @@
+function exercicio01() {
+    let indice = 0;
+
+    while (indice < 5) {
+        let nome = prompt("Digite o nome do aluno:");
+        alert("Aluno cadastrado: " + nome);
+        indice++;
+    }
+
+    alert("Cadastro finalizado");
+}
+
+function exercicio02() {
+    let indice = 0;
+
+    while (indice < 4) {
+        let cor = prompt("Digite uma cor:");
+        alert("Cor escolhida: " + cor);
+        indice++;
+    }
+    alert("Obrigado por informar suas cores favoritas");
+}
+function exercicio03() {
+    let indice = 0;
+
+    while (indice < 3) {
+        let codade = prompt("Digite o nome da cidade:");
+        alert("Destino " + (Indice + 1) + ":" + cidade);
+        indice++;
+    }
+    alert("Planejamento de viagem concluido");
+}
+
+function exercicio03() {
+    let indice = 0;
+
+    while (indice < 3) {
+        let cidade = prompt("Digite o nome da cidade:");
+        alert("Destino " + (indice + 1) + ": " + cidade);
+        indice++;
+    }
+
+    alert("Planejamento de viagem concluído");
+}
